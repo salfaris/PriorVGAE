19b829fd11565e1dc1d2fb22e36da263ba87bf9b
Salman <salmanfaris98@gmail.com>
2022-06-12 21:53:26 +0100

    Clean link prediction stuffs

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from absl import app, flags, logging
 import functools
 from typing import List, Dict, Any
 
-from dataset import load_dataset, train_val_test_split_edges, negative_sampling
+from dataset import load_dataset
 from loss import compute_vgae_loss, compute_gae_loss
 from model import gae_encoder, vgae_encoder
 
@@ -20,13 +20,6 @@ flags.DEFINE_integer('random_seed', 42, 'Random seed.')
 flags.DEFINE_integer('eval_frequency', 10, 'How often to evaluate the model.')
 flags.DEFINE_bool('is_vgae', True, 'Using Variational GAE vs vanilla GAE.')
 FLAGS = flags.FLAGS
-
-def compute_roc_auc_score(preds: jnp.ndarray,
-                          labels: jnp.ndarray) -> jnp.ndarray:
-  """Computes roc auc (area under the curve) score for classification."""
-  s = jax.nn.sigmoid(preds)
-  roc_auc = roc_auc_score(labels, s)
-  return roc_auc
 
 
 def train(dataset: List[Dict[str, Any]]) -> hk.Params:
@@ -40,19 +33,11 @@ def train(dataset: List[Dict[str, Any]]) -> hk.Params:
   
   # Get a candidate graph and label to initialize the network.
   graph = dataset[0]['input_graph']
-  train_graph, _, val_pos_s, val_pos_r, val_neg_s, val_neg_r, test_pos_s, \
-      test_pos_r, test_neg_s, test_neg_r = train_val_test_split_edges(
-      graph)
-
-  # Prepare the validation and test data.
-  val_senders = jnp.concatenate((val_pos_s, val_neg_s))
-  val_receivers = jnp.concatenate((val_pos_r, val_neg_r))
-  val_labels = jnp.concatenate(
-      (jnp.ones(len(val_pos_s)), jnp.zeros(len(val_neg_s))))
-  test_senders = jnp.concatenate((test_pos_s, test_neg_s))
-  test_receivers = jnp.concatenate((test_pos_r, test_neg_r))
-  test_labels = jnp.concatenate(
-      (jnp.ones(len(test_pos_s)), jnp.zeros(len(test_neg_s))))
+  train_graph = graph
+  train_senders = train_graph.senders
+  train_receivers = train_graph.receivers
+  train_labels = jnp.zeros(train_senders.shape)  # TODO :- Change this!
+  
   # Initialize the network.
   key, param_key = jax.random.split(key)
   params = net.init(param_key, train_graph)
@@ -69,17 +54,7 @@ def train(dataset: List[Dict[str, Any]]) -> hk.Params:
   # We jit the computation of our loss, since this is the main computation.
   compute_loss_fn = jax.jit(jax.value_and_grad(compute_loss_fn, has_aux=True))
 
-  key, *neg_sampling_keys = jax.random.split(key, FLAGS.epochs+1)
   for epoch in range(FLAGS.epochs):
-    num_neg_samples = train_graph.senders.shape[0]
-    train_neg_senders, train_neg_receivers = negative_sampling(
-        train_graph, num_neg_samples=num_neg_samples, key=neg_sampling_keys[epoch])
-    train_senders = jnp.concatenate((train_graph.senders, train_neg_senders))
-    train_receivers = jnp.concatenate(
-        (train_graph.receivers, train_neg_receivers))
-    train_labels = jnp.concatenate(
-        (jnp.ones(len(train_graph.senders)), jnp.zeros(len(train_neg_senders))))
-  
     (train_loss,
      train_preds), grad = compute_loss_fn(params, train_graph, train_senders,
                                           train_receivers, train_labels)
@@ -87,20 +62,8 @@ def train(dataset: List[Dict[str, Any]]) -> hk.Params:
     updates, opt_state = opt_update(grad, opt_state, params)
     params = optax.apply_updates(params, updates)
     if epoch % FLAGS.eval_frequency == 0 or epoch == (FLAGS.epochs - 1):
-      train_roc_auc = compute_roc_auc_score(train_preds, train_labels)
-      val_loss, val_preds = loss_fn(params, train_graph, val_senders,
-                                         val_receivers, val_labels, net)
-      val_roc_auc = compute_roc_auc_score(val_preds, val_labels)
-      logging.info(f'epoch: {epoch}, train_loss: {train_loss:.3f}, '
-            f'train_roc_auc: {train_roc_auc:.3f}, val_loss: {val_loss:.3f}, '
-            f'val_roc_auc: {val_roc_auc:.3f}')
-  test_loss, test_preds = loss_fn(params, train_graph, test_senders,
-                                       test_receivers, test_labels, net)
-  test_roc_auc = compute_roc_auc_score(test_preds, test_labels)
+      logging.info(f'epoch: {epoch}, train_loss: {train_loss:.3f}')
   logging.info('Training finished')
-  logging.info(
-      f'epoch: {epoch}, test_loss: {test_loss:.3f}, test_roc_auc: {test_roc_auc:.3f}'
-  )
   return params
 
 
