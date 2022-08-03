487239056e581bddebc37485cbe28e3c7f732575
Salman <salmanfaris98@gmail.com>
2022-08-03 01:01:34 +0100

    More flexible plotting

--- plotting.py
+++ plotting.py
@@ -40,7 +40,9 @@ def plot_images_from_arrays(
         figsize: Tuple[int, int] = (12, 4),
         image_shape: Optional[Tuple[int, int]] = None,
         cmap: str = 'viridis',
-        flip: bool = False) -> None:
+        flip: bool = False,
+        custom_min: int = None,
+        custom_max: int = None) -> None:
     if image_shape is None:
         image_shape = (15, 10)  # (num_x, num_y)
 
@@ -51,14 +53,17 @@ def plot_images_from_arrays(
         ncols, nrows = nrows, ncols
         
     fig, axes = plt.subplots(nrows, ncols, figsize=figsize)
-    _min, _max = np.amin(arrays), np.amax(arrays)
+    if custom_min is None:
+        custom_min = np.amin(arrays)   
+    if custom_max is None:
+        custom_max = np.amax(arrays)   
 
     imshow_dict = {
         'cmap': cmap,
         'interpolation': 'none',
         'origin': 'lower',
-        'vmin': _min,
-        'vmax': _max,
+        'vmin': custom_min,
+        'vmax': custom_max,
     }
     num_x, num_y = image_shape
     if len(arrays) == 1:
